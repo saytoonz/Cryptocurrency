37f439aa2186be6c896684847a0f5cf49995a56d
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2023-03-20 19:56:33 +0000

    Updated

--- app/src/main/java/com/saytoonz/cryptocurrency/data/remote/CoinPaprikaApi.kt
+++ app/src/main/java/com/saytoonz/cryptocurrency/data/remote/CoinPaprikaApi.kt
@@ -10,5 +10,5 @@ interface CoinPaprikaApi {
     suspend fun getCoins() : List<CoinDto>
 
     @GET("/v1/coins/{coinId}")
-    suspend fun getCoinById( @Path("coinId ") id: String) : CoinDetailDto
+    suspend fun getCoinById( @Path("coinId") coinId: String) : CoinDetailDto
 }
--- app/src/main/java/com/saytoonz/cryptocurrency/presentation/coin_list/CoinListScreen.kt
+++ app/src/main/java/com/saytoonz/cryptocurrency/presentation/coin_list/CoinListScreen.kt
@@ -1,5 +1,6 @@
 package com.saytoonz.cryptocurrency.presentation.coin_list
 
+import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
